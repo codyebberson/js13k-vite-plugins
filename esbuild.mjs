f9e347ce056c88267d8728d6c0d84c715329b6af
Cody Ebberson <cody@ebberson.com>
2024-03-10 13:03:39 -0700

    Version 0.3.0

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,52 +1,48 @@
 /* global console */
 /* eslint no-console: "off" */
 
-import esbuild from 'esbuild';
-import { writeFileSync } from 'fs';
+import esbuild from "esbuild";
+import { writeFileSync } from "fs";
 
 const options = {
-  entryPoints: ['./src/index.ts'],
+  entryPoints: ["./src/index.ts"],
   bundle: true,
-  platform: 'node',
-  loader: { '.ts': 'ts' },
-  resolveExtensions: ['.ts'],
-  target: 'es2021',
-  tsconfig: 'tsconfig.json',
+  platform: "node",
+  loader: { ".ts": "ts" },
+  resolveExtensions: [".ts"],
+  target: "es2021",
+  tsconfig: "tsconfig.json",
   minify: false,
   sourcemap: true,
   external: [
-    '@ampproject/rollup-plugin-closure-compiler',
-    '@rollup/plugin-typescript',
-    'advzip-bin',
-    'clean-css',
-    'ect-bin',
-    'glob',
-    'google-closure-compiler',
-    'html-minifier-terser',
-    'roadroller',
-    'terser',
-    'rollup',
-    'ts-node',
-    'tslib',
-    'typescript',
-    'vite',
+    "@rollup/plugin-typescript",
+    "advzip-bin",
+    "clean-css",
+    "ect-bin",
+    "glob",
+    "html-minifier-terser",
+    "roadroller",
+    "terser",
+    "rollup",
+    "typescript",
+    "vite",
   ],
 };
 
 esbuild
   .build({
     ...options,
-    format: 'cjs',
-    outfile: './dist/cjs/index.cjs',
+    format: "cjs",
+    outfile: "./dist/cjs/index.cjs",
   })
-  .then(() => writeFileSync('./dist/cjs/package.json', '{"type": "commonjs"}'))
+  .then(() => writeFileSync("./dist/cjs/package.json", '{"type": "commonjs"}'))
   .catch(console.error);
 
 esbuild
   .build({
     ...options,
-    format: 'esm',
-    outfile: './dist/esm/index.mjs',
+    format: "esm",
+    outfile: "./dist/esm/index.mjs",
   })
-  .then(() => writeFileSync('./dist/esm/package.json', '{"type": "module"}'))
+  .then(() => writeFileSync("./dist/esm/package.json", '{"type": "module"}'))
   .catch(console.error);
